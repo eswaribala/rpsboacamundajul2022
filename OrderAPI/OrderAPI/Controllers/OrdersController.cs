67a956a1ce1c976de99aa7a09b3a7ad941d4e633
eswaribala <parameswaribala@gmail.com>
2022-07-08 10:33:03 +0530

    updated

---
+++ OrderAPI/OrderAPI/Controllers/OrdersController.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using OrderAPI.Models;
+
+namespace OrderAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class OrdersController : ControllerBase
+    {
+        private ILogger<OrdersController> _logger;
+        public OrdersController(ILogger<OrdersController> logger)
+        {
+            _logger = logger;
+        }
+
+        // POST api/<VehicleController>
+        [HttpPost]
+        public Order Post([FromBody] Order Order)
+        {
+
+            _logger.LogInformation($"The Order received as {Order.OrderId}");
+            return Order;
+
+        }
+    }
+}
---
+++ OrderAPI/OrderAPI/Models/Order.cs
@@ -0,0 +1,9 @@
+﻿namespace OrderAPI.Models
+{
+    public class Order
+    {
+        public long OrderId { get; set; }
+        public long OrderAmount { get; set; }
+        public DateTime OrderDate { get; set; }
+    }
+}
